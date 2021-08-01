50a742c1920ce70e68ebada283a62215690bd23d
taangela <makoperek@gmail.com>
2021-08-01 13:59:45 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import List from "./List.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <p>będzie dobrze</p>
+      <List />
+    </div>
+  );
+}
---
+++ src/List.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+const ALL_DOGS_LIST = "https://dog.ceo/api/breeds/list/all";
+const BREED_URL_START = "https://dog.ceo/api/breed/";
+const BREED_URL_END = "/images/random/3";
+
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      imageURL: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${ALL_DOGS_LIST}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          //console.log('items', result);
+          this.setState({
+            isLoaded: true,
+            items: Object.keys(result.message)
+          });
+          console.log("ite", this.state.items);
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  handleClick = (breed) => {
+    console.log("key", breed);
+    fetch(`${BREED_URL_START}${breed}${BREED_URL_END}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            imageURL: result.message
+          });
+          console.log("imageURL", this.state.imageURL);
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  };
+  //https://dog.ceo/api/breed/hound/images/random Fetch!
+
+  render() {
+    const { error, isLoaded, items, imageURL } = this.state;
+    if (error) {
+      return <div>Błąd: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Ładowanie...</div>;
+    } else {
+      //console.log("state", this.state);
+      return (
+        <div>
+          <div>
+            {items.map((breed) => (
+              //zapisać sobie dlaczego to jest fajne
+              <button onClick={() => this.handleClick(breed)} key={breed}>
+                {breed}
+              </button>
+            ))}
+          </div>
+          <div>{imageURL.map}</div>
+        </div>
+      );
+    }
+  }
+}
+
+export default List;
+
+//<Button onClick={this.choseBreed}>{breed}</Button>
