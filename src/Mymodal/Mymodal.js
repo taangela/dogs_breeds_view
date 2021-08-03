9c0eba6434f343ffd22e581d9b37a9e53f9c4863
taangela <makoperek@gmail.com>
2021-08-03 21:44:36 +0200

    Add: closeButton style

--- src/Mymodal/Mymodal.js
+++ src/Mymodal/Mymodal.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
 
+import "./Mymodal.scss";
+
 class Mymodal extends React.Component {
   render() {
     console.log("mymodal:", this.props);
     return (
       <Modal
         show={this.props.show}
-        imageURL={this.props.imageURL}
+        imageURLs={this.props.imageURLs}
         onHide={this.props.handleClose}
       >
         <Modal.Header closeButton></Modal.Header>
         <Modal.Body>
-          {this.props.imageURL.map((img) => (
-            <img src={img} alt={"dog"} key={img.index} />
+          {this.props.imageURLs.map((img) => (
+            <div clasName={"imgWrapper"}>
+              <img src={img} alt={"dog"} key={img.index} />
+            </div>
           ))}
         </Modal.Body>
       </Modal>
