c40a167456066db2f7bd2afbefa9528a47fd3f11
taangela <makoperek@gmail.com>
2021-08-05 12:37:52 +0200

    Chang: components organization

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./styles/global.scss";
 
-import App from "./App/App.js";
+import App from "./components/App/App.js";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
