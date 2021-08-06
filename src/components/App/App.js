41bdf01ed6a3ebac08f4fafe82e612dfe341c54b
taangela <makoperek@gmail.com>
2021-08-06 14:41:13 +0200

    Change: page title

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,7 @@ import "./App.scss";
 export default function App() {
   return (
     <div className={"app"}>
-      <p>Dogs breed view</p>
+      <p>Dogs breed viewer</p>
       <Main />
     </div>
   );
